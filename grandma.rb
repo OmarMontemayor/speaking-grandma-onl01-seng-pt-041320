851b1a87f2c1b5a350cdbb6f45558716a632d65f
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-17 01:57:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,11 +11,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP SONNY!"
-  else 
-    return "NO, NOT SINCE 1938!"
   end  
 end
 
-speak_to_grandma("Hi")
-speak_to_grandma("HI")
-speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma("test")
