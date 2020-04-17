304408c94e48469dec4b24f5c34bc48d230b4a94
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-17 01:58:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP SONNY!"
-  end  
+  end
+  return "NO, NOT SINCE 1938!"
 end
 
 speak_to_grandma("test")
